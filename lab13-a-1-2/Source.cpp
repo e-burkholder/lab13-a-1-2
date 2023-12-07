a573a5610286d1ae4456ef71a083634468dcd1c4
Elizabeth Burkholder <e-burkholder@onu.edu>
2023-12-07 00:53:04 -0500

    pressEnterToContinue function completed.

--- lab13-a-1-2/Source.cpp
+++ lab13-a-1-2/Source.cpp
@@ -2,3 +2,20 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+
+using namespace std;
+
+void pressEnterToContinue();
+
+int main() {
+
+	return 0;
+}
+
+void pressEnterToContinue() {
+	char c;
+	cout << "Press Enter to continue...";
+	cin.ignore(1024, '\n');
+	do { cin.get(c); } while (c != '\n' && c != EOF);
+	return;
+}
